263593a786d9fe39f79503fc5c3f637ae40f3777
sek <seksemberuun@gmail.com>
2021-01-27 14:47:05 +0800

    addEventlistener Tovch darah uildel hiilee

--- app.js
+++ app.js
@@ -7,15 +7,23 @@ var scores = [0, 0];
 // Тоглогчдын ээлжийн оноог хадгалах хувьсагч
 var roundScore = 0;
 
-// Шооны аль талаара буусныг хадгалах хувьсагч, 1-6 тооны санамсаргүй
-var dice = Math.floor(Math.random() * 6) + 1;
+
 // Програм эхлэхэд бэлдэе
 document.getElementById('current-0').textContent = 0;
 document.getElementById('current-1').textContent = 0;
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
 
-document.querySelector('.dice').style.display = "none";
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = "none";
 
-// console.log('Шоо:' + dice);
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    // Шооны аль талаара буусныг хадгалах хувьсагч, 1-6 тооны санамсаргүй 
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(diceNumber);
+    diceDom.style.display = 'block'
+    diceDom.src = 'dice-' + diceNumber + '.png';
+});
+
+
